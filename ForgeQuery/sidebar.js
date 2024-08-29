3ae6d9c39d4029b064756532e0a0d966f93a0ea7
iThorgrim <contact@warlog.eu>
2024-08-29 12:27:52 +0000

    chore: update forge documentation

---
+++ ForgeQuery/sidebar.js
@@ -0,0 +1,19 @@
+document.write(`
+  <a id="ForgeQuery:GetBool" class="fn" href="../ForgeQuery/GetBool.html">GetBool</a>
+  <a id="ForgeQuery:GetColumnCount" class="fn" href="../ForgeQuery/GetColumnCount.html">GetColumnCount</a>
+  <a id="ForgeQuery:GetDouble" class="fn" href="../ForgeQuery/GetDouble.html">GetDouble</a>
+  <a id="ForgeQuery:GetFloat" class="fn" href="../ForgeQuery/GetFloat.html">GetFloat</a>
+  <a id="ForgeQuery:GetInt16" class="fn" href="../ForgeQuery/GetInt16.html">GetInt16</a>
+  <a id="ForgeQuery:GetInt32" class="fn" href="../ForgeQuery/GetInt32.html">GetInt32</a>
+  <a id="ForgeQuery:GetInt64" class="fn" href="../ForgeQuery/GetInt64.html">GetInt64</a>
+  <a id="ForgeQuery:GetInt8" class="fn" href="../ForgeQuery/GetInt8.html">GetInt8</a>
+  <a id="ForgeQuery:GetRow" class="fn" href="../ForgeQuery/GetRow.html">GetRow</a>
+  <a id="ForgeQuery:GetRowCount" class="fn" href="../ForgeQuery/GetRowCount.html">GetRowCount</a>
+  <a id="ForgeQuery:GetString" class="fn" href="../ForgeQuery/GetString.html">GetString</a>
+  <a id="ForgeQuery:GetUInt16" class="fn" href="../ForgeQuery/GetUInt16.html">GetUInt16</a>
+  <a id="ForgeQuery:GetUInt32" class="fn" href="../ForgeQuery/GetUInt32.html">GetUInt32</a>
+  <a id="ForgeQuery:GetUInt64" class="fn" href="../ForgeQuery/GetUInt64.html">GetUInt64</a>
+  <a id="ForgeQuery:GetUInt8" class="fn" href="../ForgeQuery/GetUInt8.html">GetUInt8</a>
+  <a id="ForgeQuery:IsNull" class="fn" href="../ForgeQuery/IsNull.html">IsNull</a>
+  <a id="ForgeQuery:NextRow" class="fn" href="../ForgeQuery/NextRow.html">NextRow</a>
+`);
