e546bc206745225b679f742062656972f84c72b6
iThorgrim <ithorgrim@warlog.dev>
2024-09-08 12:12:24 +0000

    Update Eluna documentation

---
+++ SpellEffectInfo/sidebar.js
@@ -0,0 +1,5 @@
+document.write(`
+  <a id="SpellEffectInfo:GetEffect" class="fn" href="../SpellEffectInfo/GetEffect.html">GetEffect</a>
+  <a id="SpellEffectInfo:GetMiscValue" class="fn" href="../SpellEffectInfo/GetMiscValue.html">GetMiscValue</a>
+  <a id="SpellEffectInfo:GetMiscValueB" class="fn" href="../SpellEffectInfo/GetMiscValueB.html">GetMiscValueB</a>
+`);
